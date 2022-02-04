d049ca7d4ab38f564b13eeffab0ad22df9f6a76f
yunosuke <yunosuke.ide@rakus-partners.co.jp>
2022-02-04 10:56:59 +0900

    ex01

---
+++ src/main/java/com/example/demo/controller/Ex01Controller.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ex01")
+public class Ex01Controller {
+	@RequestMapping("")
+	public String index() {
+		return "exam01";
+	}
+	
+	@RequestMapping("/result")
+	public String result(String name,Model model) {
+		model.addAttribute("name",name);
+		return "exam01-result";
+	}
+}
