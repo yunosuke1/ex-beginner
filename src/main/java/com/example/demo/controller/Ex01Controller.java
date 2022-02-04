532549f395c8c992bfb56be52b847d02a4e943d1
yunosuke <yunosuke.ide@rakus-partners.co.jp>
2022-02-04 11:05:42 +0900

    make exa02.html

--- src/main/java/com/example/demo/controller/Ex01Controller.java
+++ src/main/java/com/example/demo/controller/Ex01Controller.java
@@ -4,14 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 @RequestMapping("/ex01")
 public class Ex01Controller {
+	
 	@RequestMapping("")
 	public String index() {
 		return "exam01";
 	}
-	
+
 	@RequestMapping("/result")
 	public String result(String name,Model model) {
 		model.addAttribute("name",name);
