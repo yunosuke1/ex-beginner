5c888ffb1068f9d38f53a853cb1794568fd907c2
yunosuke <yunosuke.ide@rakus-partners.co.jp>
2022-02-04 11:28:53 +0900

    make Exam02Controller

--- src/main/java/com/example/demo/controller/Exam02Controller.java
+++ src/main/java/com/example/demo/controller/Exam02Controller.java
@@ -1,5 +1,35 @@
 package com.example.demo.controller;
 
-public class Exam02Controller {
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ex02")
+public class Exam02Controller {
+	@Autowired
+	private HttpSession session;
+
+	@RequestMapping("")
+	public String index() {
+		return "exam02";
+	}
+
+	@RequestMapping("/result")
+	public String result(int num1, int num2) {
+		int result = num1 + num2;
+		session.setAttribute("num1", num1);
+		session.setAttribute("num2", num2);
+		session.setAttribute("result", result);
+		
+		return "exam02-result";
+	}
+	
+	@RequestMapping("/result2")
+	public String result2() {
+		
+		return "exam02-result2";
+	}
 }
